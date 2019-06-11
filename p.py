a0e99932598fe54b07b6b892a6874dd347ec5fde
Juker1141 <juker1141@gmail.com>
2019-06-11 20:02:00 +0800

    The final version

--- p.py
+++ p.py
@@ -1,16 +1,40 @@
+import os
+#讀取檔案
+
 products = []
+
+if os.path.isfile('products.csv'):
+	print('yeah! 找到檔案了!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格\n' in line:
+				continue #繼續
+			name,price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+
+else:
+	print('找不到檔案....')
+
+
+#讓使用者輸入
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
-		break
+		break #跳出
 	price = input('請輸入價格: ')
 	products.append([name, price])
 print(products)
 
+#印出所有購買紀錄
+
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-with open('text.csv', 'w', encoding = 'utf-8') as f:
+#寫入檔案
+
+with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
